3c30de426ec1ce32ee3571a5e112baeb89bfbf9f
NamPC <namminhnam306@gmail.com>
2025-09-16 21:09:21 +0700

    fdsf

--- Network/Server/obj/Debug/net8.0-windows/EmojiPicker.g.i.cs
+++ Network/Server/obj/Debug/net8.0-windows/EmojiPicker.g.i.cs
@@ -52,7 +52,7 @@ namespace Server {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Server;component/emojipicker.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Server;V1.0.0.0;component/emojipicker.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\EmojiPicker.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
